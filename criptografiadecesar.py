1846e38819938db740e151b28e0d12521e58443e
DódeDev <45927946+jorgerodriguesdev@users.noreply.github.com>
2020-02-09 21:19:12 -0300

    Segunda Parte do arquivo criptografia de Cesar
    Critografia de Cesar

--- criptografiadecesar.py
+++ criptografiadecesar.py
@@ -10,5 +10,24 @@ def cipher(message, dir):
         else:
             m += c
     return m
-    
+    def encrypt(message):
+    return cipher(message, 1)
+
+def decrypt(message):
+    return cipher(message, -1)
+
+def main():
+    command = sys.argv[1].lower()
+    message = sys.argv[2].lower()
+
+    if command == 'encrypt':
+        print encrypt(message)
+    elif command == 'decrypt':
+        print decrypt(message)
+    else:
+        print command + ' -> command not found'
+
+if __name__ == '__main__':
+    main()
+
     
